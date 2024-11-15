d017388cbdfd581dc42e31aedea8d642c78642c9
david <bobsaggy82@gmail.com>
2024-11-15 03:51:12 -0500

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,4 @@
-local vu = game:GetService("VirtualUser")
+local vu = game:GetService("VirtualUser")--ibraham moizoos
 print("ANTIAFK ENABLED")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
    vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
